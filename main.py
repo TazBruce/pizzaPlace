47c66aa105678af574502653bc663fd0b4a2ecc1
Taz <54491493+tazbruce@users.noreply.github.com>
2019-09-04 11:29:12 +1200

    fresh start
    Signed-off-by: Taz <54491493+TazBruce@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
 import PySimpleGUI as sg
 import sqlite3
 
-layout = [[sg.Text('Your typed chars appear here:'), sg.Text('', size=(15,1), key='_OUTPUT_')],
-          [sg.Input(key='_IN_')],
-          [sg.Button('Show'), sg.Button('Exit')]]
-
-window = sg.Window('Window Title', layout)
-
-while True:  # Event Loop
-    event, values = window.Read()
-    print(event, values)
-    if event is None or event == 'Exit':
-        break
-    if event == 'Show':
-        # Update the "output" element to be the value of "input" element
-        window.Element('_OUTPUT_').Update(values['_IN_'])
-
-window.Close()
