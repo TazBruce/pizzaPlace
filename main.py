bed9612c0a15276fc5e6ed2546195eb0dcb32f6c
Taz <54491493+TazBruce@users.noreply.github.com>
2019-09-05 14:11:00 +1200

    added csv files for table testing added py file for viewing tables added base layout for order creation tab
    Signed-off-by: Taz <54491493+TazBruce@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 import PySimpleGUI as sg
-import sqlite3
 
+tab1_layout = [[sg.T('Add order')],
+               [sg.T('First Name  '), sg.Input(size=(23, 1))],
+               [sg.T('Last Name  '), sg.Input(size=(23, 1))],
+               [sg.T('Add Pizza   '), sg.InputCombo(('Ham & Cheese', 'Cheese'), size=(20, 1)), sg.Button('Add')],
+               [sg.T('Total Pizzas'), sg.InputCombo('Cheese', size=(20, 1)), sg.Button('Delete')],
+               [sg.T('Delivery?    '), sg.Checkbox('YES'), sg.T('Total Cost'), sg.T('$')]
+               ]
+
+tab2_layout = [[sg.T('This is inside tab 2')],
+               [sg.In(key='in')]]
+
+layout = [[sg.TabGroup([[sg.Tab('Create Order', tab1_layout), sg.Tab('Tab 2', tab2_layout)]])],
+          [sg.Button('Read')]]
+
+window = sg.Window('My window with tabs', layout, default_element_size=(12, 1))
+
+while True:
+    event, values = window.Read()
+    print(event, values)
+    if event is None:  # always,  always give a way out!
+        break
